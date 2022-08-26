86810f0da3a129119679c85a2415fb1dbcc568a6
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2022-08-26 18:19:51 +0300

    header

--- src/index.js
+++ src/index.js
@@ -18,3 +18,34 @@ const { height: pageHeaderHeight } = document
   .getBoundingClientRect();
 
 document.body.style.paddingTop = `${pageHeaderHeight}px`;
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('[data-page]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
+window.onscroll = () => changeHeaderBackground();
+
+function changeHeaderBackground() {
+  const header = document.querySelector('[data-header]');
+  const headerOffsetTrigger = header.offsetTop;
+  const pageOffset = window.pageYOffset;
+
+  if (pageOffset > headerOffsetTrigger) {
+    header.classList.add('no-transparency');
+  } else {
+    header.classList.remove('no-transparency');
+  }
+}
